e0ce640ddce283a08164886b00e13a9533c60cfd
Daryl Xu <ziqiang_xu@qq.com>
2020-06-02 11:23:29 +0800

    grpc interface test

--- pdf-generator/client_test.py
+++ pdf-generator/client_test.py
@@ -9,14 +9,11 @@ def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
-    with grpc.insecure_channel('localhost:50052') as channel:
+    with grpc.insecure_channel('localhost:50053') as channel:
         stub = xyunit_pb2_grpc.ReportsGeneratorStub(channel)
         response1 = stub.SayHello(xyunit_pb2.HelloRequest(Message='you'))
-        # response2 = stub.GenBrain(xyunit_pb2.BrainRequest(patientName="daryl", patientAge=10))
-        response2 = stub.GenBrainReport(xyunit_pb2.BrainReportRequest(seriesPath="",
-                                                                      patientId=""))
+        # todo test pdf generator
     print("client received: " + response1.Message)
-    print("client received: " + response2.path)
 
 
 if __name__ == '__main__':
