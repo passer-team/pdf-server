4fef1bfd50c09c514daa7d77e5a51dd8fd9aac89
Daryl Xu <xuziqiang@zyheal.com>
2021-01-26 23:37:22 +0800

    feat(config): update config example

--- pdf_server/app_config_example.py
+++ pdf_server/app_config_example.py
@@ -1,4 +1,8 @@
 # IS_DEV = False
 IS_DEV = True
 IS_DOCKER = True
-LISTEN_PORT = 50053
+LISTEN_PORT = 50054
+WORKPLACE = "/home/ziqiang_xu/zy/middle-platform/pdf-server/workplace"
+FONTS_PATH = "/home/ziqiang_xu/.liver-worker/fonts/Alibaba-PuHuiTi-Regular.ttf"
+PDF_SCRIPT = "/home/ziqiang_xu/zy/middle-platform/pdf-server/pdf.js"
+CHUNK_SIZE = 2 * 1024 * 1024
