13d27c041502900b048685e8973ac1f4b31adf9c
许自强 <ziqiang_xu@qq.com>
2022-02-19 15:52:20 +0800

    fix: Never timeout when load resources and add more readable loggings

--- pdf_server/pdf.js
+++ pdf_server/pdf.js
@@ -1,5 +1,5 @@
 /**
- * @author Daryl Xu <xuziqiang@zyheal.com>
+ * @author Daryl Xu <ziqiang_xu@qq.com>
  * convert html to pdf
  */
 // https://pptr.dev/
@@ -8,22 +8,23 @@ const puppeteer = require('puppeteer');
 
 const args = process.argv.slice(2);
 
-console.log(`${args[0]} ${args[1]}`);
+console.log(`HTML resource：${args[0]}，PDF output path：${args[1]}`);
 
 (async () => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
   // await page.goto('https://www.baidu.com', {waitUntil: 'networkidle2'});
-  // await page.goto('file:///home/ziqiang_xu/Seafile/LiverDataExample(12)/selected/01190626V001/report/resources/report.html', {waitUntil: 'networkidle2'});
+  console.log('Load the HTML')
   await page.goto(
     `file://${path.resolve(args[0])}`,
-    { waitUntil: 'networkidle2' });
+    { timeout: 0,  waitUntil: 'networkidle2' });
+  
+  console.log('Start to render HTML to PDF')
   await page.pdf({
-    // path: '/home/ziqiang_xu/Seafile/LiverDataExample(12)/selected/01190626V001/report/report.pdf', 
     path: args[1],
     format: 'A4',
     printBackground: true
   });
-
   await browser.close();
+  console.log('Render finished and close the browser!')
 })();
